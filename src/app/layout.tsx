fac2e953197a25bd2ed19fe803ac2d30a5e3ca79
KarkiAdit <adityakarki728@gmail.com>
2024-12-24 17:33:27 -0600

    Added some global CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
 import "./globals.css";
 import Providers from '@/app/providers';
+
+// Use Figtree font
+const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Google Devs Fisk",
@@ -15,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="container mx-auto px-4 max-w-6xl">
+      <div className={`container mx-auto px-8 sm:px-4 max-w-screen-lg ${figtree.className}`}>
           <Providers>
             {children}
           </Providers>
