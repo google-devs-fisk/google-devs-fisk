631df477739354a06bab3d35c0b7f5e9f3ca6c61
KarkiAdit <adityakarki728@gmail.com>
2025-01-10 02:45:54 -0600

    introduced lastUpdated for implementing on deman ISR data fetch

--- src/types/culture.ts
+++ src/types/culture.ts
@@ -1,7 +1,9 @@
 import { Testimonial, Metric, VideoContent} from './common';
+import FirebaseFirestore from "@google-cloud/firestore";
 
 export interface CulturePageResponse {
     culturePageVideo: VideoContent;
     testimonials: Testimonial[];
     metrics: Metric[];
+    lastUpdated: FirebaseFirestore.Timestamp;
 }
